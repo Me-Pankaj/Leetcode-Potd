ac22b8428144aae6569394fdb7043c84571af9dc
Pankaj Gupta <118957086+Me-Pankaj@users.noreply.github.com>
2023-02-13 09:52:00 +0530

    Time: 3 ms (66.30%), Space: 5.9 MB (63.61%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -11,17 +11,9 @@ public:
         {
             return (h/2)-(l/2);
         }
-        else if(l%2==0 and h%2==1)
-        {
-            int x=(l/2);
-            int y=(h/2)+1;
-            return y-x;
-        }
-        else
-        {
-            int x=(l/2)+1;
-            int y=(h/2);
-            return y-x+1;
+        else {
+            
+            return 1+ abs(h-l)/2;
         }
         return -1;
     }
