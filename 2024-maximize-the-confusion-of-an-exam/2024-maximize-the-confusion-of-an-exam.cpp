77a0686d059ac3e0f0122d94b6a665a6c66ba3bf
Pankaj Gupta <118957086+Me-Pankaj@users.noreply.github.com>
2023-02-18 10:15:49 +0530

    Time: 81 ms (14.06%), Space: 10.2 MB (42.61%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxConsecutiveAnswers(string s, int k) {
+        int n=s.size();
+        int i=0,j=0,ans=0;
+        int maxi=0;
+        map<int,int>m;
+        while(j<n)
+        {
+            m[s[j]]++;
+            maxi=max(maxi,m[s[j]]);
+            if((j-i+1)-maxi>k)
+            {
+                m[s[i]]--;
+                maxi=max(maxi,m[s[i]]);
+                i++;
+            }
+            ans=max(ans,j-i+1);
+            j++;
+        }
+        return ans;
+    }
+};
